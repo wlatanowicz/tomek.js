3a2372532cb7bdf287c18819b5550af74df71d35
Wiktor Latanowicz <wlatanowicz@flux.pl>
2013-03-31 15:40:24 +0000

    parsowanie wyrazen
    git-svn-id: svn://dev.flux.pl/wiktor/tomekjs/trunk@26 efa8fd9f-5dd9-4d94-bdc3-34ac9bdffb1a

--- src/jef/compiler/template/text_expression.rb
+++ src/jef/compiler/template/text_expression.rb
@@ -5,18 +5,70 @@ require 'json'
 
 class TextExpression
  
+	class JSString < String
+		
+	end
+	
+	class JSExpression < String
+		
+	end
+	
 	@str
 	
 	def initialize str
-    @str = str.to_s
+		parts = []
+		
+		mode = "text"
+		part = ""
+		
+		i = 0
+		str = str.to_s.strip
+		l = str.length
+		
+		while i < ( l + 1 ) do
+		
+			if mode == "text" and ( i >= l or str[i,3] == "[%=" ) then
+				mode = "expr"
+				i += 3
+				
+				part = part.strip
+				if part.length > 0 then
+					part = part.to_json
+					parts.push part
+				end
+				
+				part = ""
+			end
+			
+			if mode == "expr" and ( i >= l or str[i,2] == "%]" ) then
+				mode = "text"
+				i += 2
+				
+				part = part.strip
+				if part.length > 0 then
+					part = "("+part+")"
+					parts.push part
+				end
+				
+				part = ""
+			end
+			
+			if ( i < l ) then
+				part += str[i,1]
+			end
+			
+			i += 1
+		end
+
+    @str = parts.join( "+" )
   end
 	
 	def to_s
-		@str.strip
+		@str
 	end
 	
 	def js_expression
-		escape_js_string @str.strip
+		@str
 	end
 	
 	def escape_js_string str
