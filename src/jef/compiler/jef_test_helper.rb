e7a29a6dc747eb1ea69c3e36f2c19a713946a847
Wiktor Latanowicz <wlatanowicz@flux.pl>
2013-10-04 15:53:01 +0000

    generator listy testow
    git-svn-id: svn://dev.flux.pl/wiktor/tomekjs/trunk@90 efa8fd9f-5dd9-4d94-bdc3-34ac9bdffb1a

--- src/jef/compiler/jef_test_helper.rb
+++ src/jef/compiler/jef_test_helper.rb
@@ -8,4 +8,34 @@ class JefTestHelper < JefHelper
 		@BUILD_DIR     = File.join(ROOT_DIR, 'test_build')
 	end
 
+	def index_file
+		return File.join( @BUILD_DIR, 'index.html' )
+	end
+	
+	def update_tests
+		
+		
+		file = File.join(@APP_DIR, APP_YML)
+		app = YAML.load_file( file )
+		
+		newapp = Hash.new
+		
+		test_list = []
+		test_dir = File.join( @APP_DIR, 'tests' )
+		
+		Dir.foreach(test_dir) do |f|
+			test_list.push File.join( "tests", f, "main.js" ) if ! f.start_with?( '.' )
+		end
+		
+		newapp['MAINS'] = test_list
+		newapp['TEMPLATES'] = app['TEMPLATES']
+		newapp['RESOURCES'] = app['RESOURCES']
+
+		
+		File.open( file, 'w' ) do |io|
+			YAML.dump( newapp, io )
+		end
+		
+ 	end
+	
 end
