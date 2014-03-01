51911ac7dbe1705e88085f4c6c6cfa964d8358bb
Wiktor Latanowicz <wlatanowicz@flux.pl>
2014-03-01 22:26:14 +0000

    komentarz z lista gemow
    git-svn-id: svn://dev.flux.pl/wiktor/tomekjs/trunk@148 efa8fd9f-5dd9-4d94-bdc3-34ac9bdffb1a

--- src/tomek.js/compiler/jef_doc_helper.rb
+++ src/tomek.js/compiler/jef_doc_helper.rb
@@ -1,6 +1,9 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
+# gem install treetop
+# gem install bluecloth
+
 class JefDocHelper < JefHelper
 
 	DOC_DIR = File.join(ROOT_DIR, 'doc')
