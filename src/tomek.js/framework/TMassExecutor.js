8567d8711c957aa5bcdbabe850538637212a5986
Wiktor Latanowicz <wlatanowicz@flux.pl>
2014-04-05 15:39:28 +0000

    obiekt do wykonywania metod na całej tablicy done50 #268
    git-svn-id: svn://dev.flux.pl/wiktor/tomekjs/trunk@203 efa8fd9f-5dd9-4d94-bdc3-34ac9bdffb1a

---
+++ src/tomek.js/framework/TMassExecutor.js
@@ -0,0 +1,45 @@
+//= require TObject
+
+/** section: Utilities
+ * class TMassExecutor
+ * 
+ **/
+klass( 'TMassExecutor', {
+	
+	___array : [],
+	
+	constructor : function( array ){
+		if ( array ){
+			var i;
+			for( i=0; i<array.length; i++ ){
+				this.___addObject( array[i] );
+			}
+		}
+	},
+	
+	___addObject : function( object ){
+		this.___array.push( object );
+		for ( var methodName in object ){
+			try {
+			  if (typeof(object[methodName]) == "function") {
+				  this[methodName] = this.___invokeMethod.bind( this, methodName );
+			  }
+			} catch (err) {
+			}
+		}
+	},
+	
+	___invokeMethod : function( methodName ){
+		var methodArgs = [];
+		var i;
+		for ( i=1; i<arguments.length; i++ ){
+			methodArgs.push( arguments[i] );
+		}
+		for ( i=0; i<this.___array.length; i++ ){
+			if ( typeof(this.___array[i][methodName]) == "function" ){
+				this.___array[i][methodName].apply( this.___array[i], methodArgs );
+			}
+		}
+	}
+	
+});
