837a4192b802a131f1e07af7051683cb6002dd7c
Wiktor Latanowicz <wlatanowicz@flux.pl>
2015-10-20 15:21:54 +0200

    usuniecie zbednych plikow

--- src/tomek.js/compiler.js/compiler.js
+++
@@ -1,3 +0,0 @@
-var Parser_1 = require("./parser/Parser");
-var parser = new Parser_1["default"]();
-parser.parseFile("../app/TMyControl.tpl");
--- src/tomek.js/compiler.js/template/EventAttribute.js
+++
@@ -1,7 +0,0 @@
-var EventAttribute = (function () {
-    function EventAttribute(xmlNode) {
-    }
-    return EventAttribute;
-})();
-exports.__esModule = true;
-exports["default"] = EventAttribute;
