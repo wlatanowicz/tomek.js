44a8b31ed62d722bf5596212adaaf544b69f8365
Wiktor Latanowicz <wlatanowicz@flux.pl>
2015-11-24 10:29:39 +0100

    lepsze skrypty

--- src/tomek.js/compiler.js/build.sh
+++ src/tomek.js/compiler.js/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-tsc --module commonjs build.ts && node build.js
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$DIR/node_modules/tsc/bin/tsc --module commonjs build.ts && node build.js $@
---
+++ src/tomek.js/compiler.js/get_dependencies.sh
@@ -0,0 +1,3 @@
+npm install tsc
+npm install libxmljs glob minimist md5
+
