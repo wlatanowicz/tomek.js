823b6a0334236bb34b17554413546208a65a0935
Wiktor Latanowicz <wlatanowicz@flux.pl>
2016-04-28 14:40:24 +0200

    auto get deps on build, test etc.

--- src/tomek.js/compiler/build.sh
+++ src/tomek.js/compiler/build.sh
@@ -4,6 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd $DIR > /dev/null
 
+if [ ! -d "$DIR/node_modules" ]
+then
+  $DIR/get_dependencies.sh
+fi
+
 $DIR/node_modules/tsc/bin/tsc --module commonjs build.ts && node build.js $@
 
 ec=$?
