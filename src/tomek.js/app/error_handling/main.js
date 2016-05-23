83d7f0d5f31b78cf888d1bfb5a3d12457e5d7584
Wiktor Latanowicz <wlatanowicz@flux.pl>
2016-05-23 17:04:10 +0200

    order fix :-)

--- src/tomek.js/app/error_handling/main.js
+++ src/tomek.js/app/error_handling/main.js
@@ -3,9 +3,10 @@
 //= require CustomErrorHandler
 //= require ErrorHandling-tpl
 
-var c = new ErrorHandling( { 'Placeholder' : 'container' } );
-c.render();
-
 TErrorRouter.register();
 TErrorRouter.addHandler( new TErrorHandler() );
 TErrorRouter.addHandler( new CustomErrorHandler() );
+
+var c = new ErrorHandling( { 'Placeholder' : 'container' } );
+c.render();
+
