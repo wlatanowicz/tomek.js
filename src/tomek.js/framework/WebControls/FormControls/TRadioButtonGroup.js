87855c2ae5aa56b9d5212d56eaa9ee88329f163e
Wiktor Latanowicz <wlatanowicz@flux.pl>
2016-05-30 14:25:14 +0200

    radio button group

--- src/tomek.js/framework/WebControls/FormControls/TRadioButtonGroup.js
+++ src/tomek.js/framework/WebControls/FormControls/TRadioButtonGroup.js
@@ -7,12 +7,29 @@
  * Control fetches value from radio button inputs
  * 
  **/
-klass( 'TRadioButton', TControl, {
+klass( 'TRadioButtonGroup', TControl, {
 	
 	setValue : function( v ){
+		var btns = this.$$kind( TRadioButton );
+		var checked = false;
+		var group = this.getGroup();
+		for ( var i=0; !checked && i<btns.length; i++ ){
+			if ( btns[i].getValue() == v && ( group == '' || btns[i].getGroup() == group ) ){
+				btns[i].setChecked(true);
+				checked = true;
+			}
+		}
 	},
 	
 	getValue : function(){
+		var btns = this.$$kind( TRadioButton );
+		var group = this.getGroup();
+		for ( var i=0; i<btns.length; i++ ){
+			if ( btns[i].getChecked() && ( group == '' || btns[i].getGroup() == group ) ){
+				return btns[i].getValue();
+			}
+		}
+		return null;
 	},
 	
 	/**
