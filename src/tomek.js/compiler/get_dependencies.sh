5c8f88bbb5ec470e11173667dafb533028b6b644
Wiktor Latanowicz <wlatanowicz@flux.pl>
2016-06-27 11:02:52 +0200

    cleanup 3rdparty libs

--- src/tomek.js/compiler/get_dependencies.sh
+++ src/tomek.js/compiler/get_dependencies.sh
@@ -5,13 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 
 npm install tsc
-
-npm install object.observe
-cp node_modules/object.observe/dist/object-observe.js ../compatibility/Object-observe.js
-
-npm install es6-promise-polyfill
-cp node_modules/es6-promise-polyfill/promise.js ../compatibility/Promise.js
-
 npm install libxmljs glob minimist md5 mkdirp fs-extra uglify-js ndoc
 
 popd
