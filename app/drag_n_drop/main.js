3c3fd797aa8f5e2369368d233236d3cdf8182d9e
Wiktor Latanowicz <wlatanowicz@flux.pl>
2017-04-08 17:34:50 +0200

    drag n drop

--- app/drag_n_drop/main.js
+++
@@ -1,5 +0,0 @@
-
-//= require DragDrop-tpl
-
-var c = new DragDrop( { 'Placeholder' : 'container' } );
-c.render();
---
+++ app/drag_n_drop/main.ts
@@ -0,0 +1,6 @@
+
+import DragDrop from "@app/drag_n_drop/DragDrop";
+
+var c = new DragDrop();
+c.Placeholder = "container";
+c.render();
